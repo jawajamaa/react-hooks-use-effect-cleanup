357ebde6d17d88fdc852cb9068b7fddba568340a
Timothy Ryon <tim@timryon.com>
2024-02-03 19:24:42 -0500

    finished codealong

---
+++ App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+function App() {
+const [showClock, setShowClock] = useState(true);
+
+
+    return (
+       <div>
+        {showClock ? <Clock /> : null}
+        <button onClick = {() => setShowClock(!showClock)}>Toggle Clock</button>
+       </div> 
+    )
+}
+
+export default App;
